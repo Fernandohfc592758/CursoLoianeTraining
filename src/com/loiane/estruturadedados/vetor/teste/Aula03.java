3d6ed1f9ea972465adaec42658e74425884a5400
FernandoH Fonseca <fernandohfc@outlook.com>
2022-10-12 23:38:29 -0300

    aula3curso

---
+++ src/com/loiane/estruturadedados/vetor/teste/Aula03.java
@@ -0,0 +1,15 @@
+package com.loiane.estruturadedados.vetor.teste;
+
+
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		Vetor vetor = new Vetor(5);
+		
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+
+	}
+
+}
